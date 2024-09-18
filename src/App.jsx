5557a8269df6c5038b848fdac51f2dea4d1023fe
Ashmit0405 <173537273+Ashmit0405@users.noreply.github.com>
2024-09-19 05:23:13 +0530

    Added some styling to footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,36 @@
-import React,{ useState,useEffect }from 'react'
-import authserv from './appwrite/auth'
-import { useDispatch } from 'react-redux'
-import './App.css'
-import {login,logout} from './store/authslice'
-import { Footer,Header } from './Components/index'
-import { Outlet } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import authserv from './appwrite/auth';
+import { useDispatch } from 'react-redux';
+import './App.css';
+import { login, logout } from './store/authslice';
+import { Footer, Header } from './Components/index';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  const [loading, setloading] = useState(false)
-  const dispatch=useDispatch();
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
+  useEffect(() => {
     authserv.getcurrentuser()
-    .then((userdata)=>{
-      if (userdata) {
-        dispatch(login({userdata}))
-      } else {
-        dispatch(logout());
-      } 
-    })
-    .finally(()=>setloading(false));
-  },[])
+      .then((userdata) => {
+        if (userdata) {
+          dispatch(login({ userdata }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, [dispatch]);
 
-  return !loading?(
-    <div className='min-h-screen flex flex-wrap content-between bg-cyan-400 w-full'>
-      <div className='w-full block'>
-        <Header/>
-        <main>
-          <Outlet/> 
-        </main>
-        <Footer/>
-      </div>
+  return !loading ? (
+    <div className="relative min-h-screen flex flex-col bg-cyan-400 w-full">
+      <Header className="top-0"/>
+      <main className="flex-grow w-full">
+        <Outlet />
+      </main>
+      <Footer className="bottom-0"/>
     </div>
-  ):null
+  ) : null;
 }
 
-export default App
+export default App;
